9e153b875fd765a704ee2a693b556560c80a8113
kyle felter <felterkyle94@gmail.com>
2021-05-02 13:37:10 -0400

    created client to send an event

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
 
 	pb "github.com/kfelter/grpc-example/eventstore"
-	"google.golang.org/protobuf/proto"
+	"google.golang.org/grpc"
+)
+
+var (
+	serverAddr = flag.String("server-addr", "localhost:10000", "the address of the server host:port")
 )
 
 func main() {
-	e := pb.Event{
+	flag.Parse()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	conn, err := grpc.Dial(*serverAddr, opts...)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	client := pb.NewEventStoreClient(conn)
+	stream, err := client.StoreEvents(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	err = stream.Send(&pb.Event{
 		Content: []byte(`new event`),
 		Tags:    []string{"environment:test"},
+	})
+	if err != nil {
+		panic(err)
 	}
-	fmt.Println(proto.Marshal(&e))
+	reply, err := stream.CloseAndRecv()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(reply.String())
 }
