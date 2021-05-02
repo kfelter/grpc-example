d1892c5e47af5dc94ce548ed8240e175cfaebf4f
kyle felter <felterkyle94@gmail.com>
2021-05-02 12:17:28 -0400

    client initial commit

---
+++ cmd/client/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	pb "github.com/kfelter/grpc-example/eventstore"
+	"google.golang.org/protobuf/proto"
+)
+
+func main() {
+	e := pb.NewEvent{
+		Content: []byte(`new event`),
+		Tags:    []string{"environment:test"},
+	}
+	fmt.Println(proto.Marshal(&e))
+}
