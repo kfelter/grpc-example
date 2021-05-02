80b7ce617e9147378b0482c981ee9943c88a3da1
kyle felter <felterkyle94@gmail.com>
2021-05-02 12:40:16 -0400

    implement StoreEvents

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"sync"
 
 	pb "github.com/kfelter/grpc-example/eventstore"
@@ -9,8 +10,9 @@ import (
 
 type eventStoreServer struct {
 	pb.UnimplementedEventStoreServer
-	mu     sync.Mutex
-	events []*pb.Event
+	mu        sync.Mutex
+	events    []*pb.Event
+	idCounter int64
 }
 
 func (s *eventStoreServer) GetEvents(req *pb.GetEventRequest) error {
@@ -19,7 +21,29 @@ func (s *eventStoreServer) GetEvents(req *pb.GetEventRequest) error {
 }
 
 func (s *eventStoreServer) StoreEvents(stream pb.EventStore_StoreEventsServer) (*pb.StoreEventsResponse, error) {
-	return nil, nil
+	events := []*pb.Event{}
+	for {
+		newevent, err := stream.Recv()
+		if err == io.EOF {
+			return &pb.StoreEventsResponse{
+				Events: events,
+				Status: fmt.Sprintf("added %d events", len(events)),
+			}, nil
+		}
+		if err != nil {
+			return nil, err
+		}
+		s.mu.Lock()
+		event := &pb.Event{
+			Id:      s.idCounter,
+			Tags:    newevent.GetTags(),
+			Content: newevent.GetContent(),
+		}
+		events = append(events, event)
+		s.events = append(s.events, event)
+		s.idCounter++
+		s.mu.Unlock()
+	}
 }
 
 func main() {
